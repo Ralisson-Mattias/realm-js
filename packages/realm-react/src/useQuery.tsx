6cae62905461f0f070e6b8739cced33e679569a0
levipro <47224088+levipro@users.noreply.github.com>
2023-02-28 07:12:00 -0800

    allow useQuery to filter or sort a collection by using a callback (#5465)
    * allow useQuery to filter or sort a collection by using a callback method that gets momoized based on the dependency array
    
    * cleanup based on comments
    
    * add comment about 'queryCallback'
    
    * update documentation
    
    * change to useCallback
    
    * cleanup comment

--- packages/realm-react/src/useQuery.tsx
+++ packages/realm-react/src/useQuery.tsx
@@ -17,9 +17,14 @@
 ////////////////////////////////////////////////////////////////////////////
 
 import Realm from "realm";
-import { useEffect, useReducer, useMemo, useRef } from "react";
+import { useEffect, useReducer, useMemo, useRef, useCallback } from "react";
 import { createCachedCollection } from "./cachedCollection";
 import { symbols } from "@realm/common";
+
+type RealmResults<T> = Realm.Results<T & Realm.Object>;
+type RealmObjectType<T> = string | ({ new (...args: any): T } & Realm.ObjectClass);
+type QueryCallback<T> = (collection: RealmResults<T>) => RealmResults<T>;
+type DependencyList = ReadonlyArray<unknown>;
 
 /**
  * Generates the `useQuery` hook from a given `useRealm` hook.
@@ -29,25 +34,36 @@ import { symbols } from "@realm/common";
  */
 export function createUseQuery(useRealm: () => Realm) {
   return function useQuery<T>(
-    type: string | ({ new (...args: any): T } & Realm.ObjectClass),
-  ): Realm.Results<T & Realm.Object> {
+    type: RealmObjectType<T>, 
+    query: QueryCallback<T> = (collection) => collection, 
+    deps: DependencyList = [],
+  ): RealmResults<T> {
     const realm = useRealm();
+
+    // We want the user of this hook to be able pass in the `query` function inline (without the need to `useCallback` on it)
+    // This means that the query function is unstable and will be a redefined on each render of the component where `useQuery` is used
+    // Therefore we use the `deps` array to memoize the query function internally, and only use the returned `queryCallback`
+    const queryCallback = useCallback(query, deps);
+
+    const queryResult = useMemo(() => {
+      return queryCallback(realm.objects(type));
+    }, [type, realm, queryCallback]);
 
     // Create a forceRerender function for the cachedCollection to use as its updateCallback, so that
     // the cachedCollection can force the component using this hook to re-render when a change occurs.
     const [, forceRerender] = useReducer((x) => x + 1, 0);
-    const collectionRef = useRef<Realm.Results<T & Realm.Object>>();
+    const collectionRef = useRef<RealmResults<T>>();
     const updatedRef = useRef(true);
 
-    // Wrap the cachedObject in useMemo, so we only replace it with a new instance if `primaryKey` or `type` change
+    // Wrap the cachedObject in useMemo, so we only replace it with a new instance if `realm` or `queryResult` change
     const { collection, tearDown } = useMemo(() => {
       return createCachedCollection({
-        collection: realm.objects(type),
+        collection: queryResult,
         realm,
         updateCallback: forceRerender,
         updatedRef,
       });
-    }, [type, realm]);
+    }, [realm, queryResult]);
 
     // Invoke the tearDown of the cachedCollection when useQuery is unmounted
     useEffect(() => {
@@ -58,14 +74,14 @@ export function createUseQuery(useRealm: () => Realm) {
     // Also we are ensuring the type returned is Realm.Results, as this is known in this context
     if (updatedRef.current) {
       updatedRef.current = false;
-      collectionRef.current = new Proxy(collection as Realm.Results<T & Realm.Object>, {});
+      collectionRef.current = new Proxy(collection as RealmResults<T>, {});
       // Store the original, unproxied result as a non-enumerable field with a symbol
       // key on the proxy object, so that we can check for this and get the original results
       // when passing the result of `useQuery` into the subscription mutation methods
       // (see `lib/mutable-subscription-set.js` for more details)
       // TODO: We can remove this if `realm` becomes a peer dependency >= 12
       Object.defineProperty(collectionRef.current, symbols.PROXY_TARGET, {
-        value: realm.objects(type),
+        value: queryResult,
         enumerable: false,
         configurable: false,
         writable: true,
@@ -73,6 +89,6 @@ export function createUseQuery(useRealm: () => Realm) {
     }
 
     // This will never not be defined, but the type system doesn't know that
-    return collectionRef.current as Realm.Results<T & Realm.Object>;
+    return collectionRef.current as RealmResults<T>;
   };
 }
